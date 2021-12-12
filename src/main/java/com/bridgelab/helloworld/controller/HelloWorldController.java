ebb906f7e34c425c56c26fc4b33cc759098098f1
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-12-12 14:56:58 +0530

    Added: a new method for UC3 of Get request which can print custom message on the local server page.

--- src/main/java/com/bridgelab/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/bridgelab/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.bridgelab.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
 
-	@GetMapping("/home")
+	@GetMapping(value = {"/","/home"})
 	public String sayHello() {
 		return "hello";
+	}
+
+	@GetMapping("/home/message")
+	public String message(Model model) {
+		model.addAttribute("message", "This is custom message from Amit Sisodiya");
+		return "message";
 	}
 }
